e9c958914854ed4e94d00e70c0d7623a7fcb58bd
KirillAnB <it-reso@mail.ru>
2024-01-21 13:49:41 +0300

    user in list

--- tests/test_users.py
+++ tests/test_users.py
@@ -16,4 +16,27 @@ def test_get_user_by_id(create_delete_dummy_user):
     result = requests.get(url=base_url + f"user/{user_id}", headers=dummyapi_headers)
     print(result.text)
 
+def test_user_in_users_list(create_delete_dummy_user):
+    user_id = create_delete_dummy_user.json().get("id")
+    user_created = create_delete_dummy_user.json().get("registerDate")
+    user_list = []
+    for i in range(1,3):
+        payload = {
+        "registerDate" : {user_created},
+        "limit" : 50,
+        "page" : i
+    }
+        user_data_list = requests.get(url=base_url + "user", headers=dummyapi_headers, params=payload).json().get("data")
+        user_list.append(user_data_list)
+    user_found = False
+    for item in user_list:
+        for _dict in item:
+            if _dict['id'] == user_id:
+                user_found = True
+                print(f"User was found on page {i}")
+    assert user_found == True
+
+
+
+
 
